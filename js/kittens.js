3a3dbfbe0704778fb2c943391394d09e77b81540
SSaquif <ssaquif@gmail.com>
2019-02-02 19:13:39 -0500

    Added up and down movement directions, modularized collisions, bullet template created

--- js/kittens.js
+++ js/kittens.js
@@ -1,4 +1,4 @@
-// This sectin contains some game constants. It is not super interesting
+// This sectin contains some game constants
 var GAME_WIDTH = 375;
 var GAME_HEIGHT = 500;
 
@@ -10,14 +10,23 @@ var MAX_LIVES = 500;
 var PLAYER_WIDTH = 75;
 var PLAYER_HEIGHT = 54;
 
+var AMMO_WIDTH = 1;
+var AMMO_HEIGHT = 1;
+
+
+
 // These two constants keep us from using "magic numbers" in our code
 var LEFT_ARROW_CODE = 37;
+var UP_ARROW_CODE = 38;
 var RIGHT_ARROW_CODE = 39;
+var DOWN_ARROW_CODE = 40;
 var SPACE_ARROW_CODE = 32;
 
 // These two constants allow us to DRY
 var MOVE_LEFT = 'left';
 var MOVE_RIGHT = 'right';
+var MOVE_UP = 'up';
+var MOVE_DOWN = 'down';
 
 
 
@@ -33,10 +42,11 @@ var images = {};
 
 
 
-// This section is where you will be doing most of your coding
+//superclass entity
 class Entity {
     render(ctx)
     {
+    		//variables sprite, x and y to be defined in subclass
         ctx.drawImage(this.sprite, this.x, this.y);
     }
 }
@@ -47,14 +57,28 @@ class Enemy extends Entity {
         this.x = xPos;
         this.y = -ENEMY_HEIGHT;
         this.sprite = images['enemy.png'];
-
         // Each enemy should have a different speed
         this.speed = Math.random() / 2 + 0.25;
     }
 
+    //updates the location of the enemy
     update(timeDiff) {
         this.y = this.y + timeDiff * this.speed;
     }
+}
+
+class ammo extends Entity {
+	constructor(xPos,yPos){
+		super();
+		this.x = xPos+(PLAYER_WIDTH/2);
+		this.y = yPos+PLAYER_HEIGHT;
+		this.sprite = images['bullet.png'];
+		this.speed = Math.random() / 2 + 0.25;
+	}
+
+	update(timeDiff){
+		this.y = this.y - timeDiff * this.speed;
+	}
 }
 
 class Player extends Entity {
@@ -69,11 +93,21 @@ class Player extends Entity {
     // This method is called by the game engine when left/right arrows are pressed
     move(direction) {
         if (direction === MOVE_LEFT && this.x > 0) {
-            this.x = this.x - 1;
+            this.x = this.x - 5;
+            console.log("X:"+this.x);
         }
         else if (direction === MOVE_RIGHT && this.x < GAME_WIDTH - PLAYER_WIDTH) {
-            this.x = this.x + 1;
+            this.x = this.x + 5;
+						console.log("X:"+this.x);
         }
+        else if (direction === MOVE_UP && this.y > 0){
+        		this.y = this.y - 5;
+						console.log("Y:"+this.y);
+				}
+				else if (direction === MOVE_DOWN && this.y < GAME_HEIGHT - PLAYER_HEIGHT){
+						this.y = this.y + 5;
+						console.log("Y:"+this.y);
+				}
     }
 }
 class Sound {
@@ -127,7 +161,7 @@ class Engine {
      At any point in time there can be at most MAX_ENEMIES enemies otherwise the game would be impossible
      */
     setupEnemies() {
-        console.log(this.enemies);
+				//create enemies array if not already setup
         if (!this.enemies) {
             this.enemies = [];
         }
@@ -137,18 +171,32 @@ class Engine {
         }
     }
 
-    // This method finds a random spot where there is no enemy, and puts one in there
-    addEnemy() {
-        var enemySpots = GAME_WIDTH / ENEMY_WIDTH;
 
-        var enemySpot;
-        // Keep looping until we find a free enemy spot at random
-        while (this.enemies[enemySpot]) {
-            enemySpot = Math.floor(Math.random() * enemySpots);
-        }
+		// This method finds a random spot where there is no enemy, and puts one in there
+		addEnemy() {
+			var enemySpots = GAME_WIDTH / ENEMY_WIDTH;
 
-        this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH);
-    }
+			var enemySpot;
+			// Keep looping until we find a free enemy spot at random
+			while (this.enemies[enemySpot]) {
+				enemySpot = Math.floor(Math.random() * enemySpots);
+			}
+			//
+			this.enemies[enemySpot] = new Enemy(enemySpot * ENEMY_WIDTH);
+		}
+
+		setupBullets() {
+			var maxBullets = GAME_WIDTH*GAME_HEIGHT; //as each bullet can at most ocuppy q pixel
+			if(!this.bullets){
+				this.bullets = [];
+				this.bullets.length = maxBullets; //max bullet array length
+			}
+		}
+
+		addBullet(){
+
+		}
+
 
     // This method kicks off the game
     start() {
@@ -164,6 +212,15 @@ class Engine {
             else if (e.keyCode === RIGHT_ARROW_CODE) {
                 this.player.move(MOVE_RIGHT);
             }
+            else if (e.keyCode === UP_ARROW_CODE){
+            		this.player.move(MOVE_UP);
+						}
+						else if (e.keyCode === DOWN_ARROW_CODE){
+            	this.player.move(MOVE_DOWN);
+						}
+						else if (e.keyCode === SPACE_ARROW_CODE){
+
+						}
             else if (e.keyCode === SPACE_ARROW_CODE && this.isRestart)
             {
                 //this.score = 0;
@@ -188,12 +245,15 @@ class Engine {
         
         var currentFrame = Date.now();
         var timeDiff = currentFrame - this.lastFrame;
-        //this.bgSound.play();
-        // Increase the score!
-        this.score += timeDiff;
+        //this.bgSound.play();//
+				//Increase the score
+				this.score += timeDiff;
 
         // Call update on all enemies
         this.enemies.forEach(enemy => enemy.update(timeDiff));
+
+        //Call update on all bullets
+				this.bullets.forEach(bullet=> bullet.update(timeDiff));
 
         // Draw everything!
         this.ctx.drawImage(images['stars.png'], 0, 0); // draw the star bg
@@ -215,13 +275,12 @@ class Engine {
             this.ctx.fillStyle = '#ffffff';
             this.ctx.fillText(this.score + ' GAME OVER', 5, 30);
             document.addEventListener('keydown', e => {
-            if (e.keyCode === SPACE_ARROW_CODE)
-            {
+            if (e.keyCode === SPACE_ARROW_CODE) {
                 this.score = 0;
                 this.player.lives = MAX_LIVES;
                 this.isRestart = 1;
-            }
-        });
+            	}
+        	});
         }
         else {
             // If player is not dead, then draw the score
@@ -235,35 +294,50 @@ class Engine {
         }
     }
 
+    colission(objA,objB,objAWeidth,objAHeight,objBWeidth,objBHeigth,){
+    	console.log("objA:"+objA.x+","+objA.y);
+    	console.log("objB:"+objB.x+","+objB.y);
+			//What you tried didnt work (vertical and horizontal separation)
+			//Reminder, Distance calculations is for circles
+			//For Rectangular (axis aligned bounded box) collision detection	(Comparing boundaries)
+			//https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detection#Axis-Aligned_Bounding_Box
+			//https://www.youtube.com/watch?v=8b_reDI7iPM GOOD VIDEO for interviews
+    	if(((objA.x + objAWeidth)> objB.x)//Player right > Eenemy Left
+				&&(objA.x < objB.x + objBWeidth)//Palyer left < Enemy right
+				&&((objA.y + objAHeight)>objB.y)//Palyer left < Enemy right
+				&&(objA.y < objB.y + objBHeigth)//Player bottom < Enemy top
+			){
+				return true;
+			}
+			return false;
+    }
+
+
     isPlayerDead() {
         // TODO: fix this function!
-        for(var i = 0; i < this.enemies.length; i++)
-        {
-            console.log(this.enemies[i]);
-            // console.log(this.enemies[i].x);
-            // console.log(this.player.x);
-            if(this.enemies[i] !== undefined)
-            {
-            if (((this.player.x + PLAYER_WIDTH) > (this.enemies[i].x))
-				&& ((this.player.x) < (this.enemies[i].x + ENEMY_WIDTH))
-				&& ((this.player.y + PLAYER_HEIGHT) > (this.enemies[i].y))
-				&& ((this.player.y) < (this.enemies[i].y + ENEMY_HEIGHT/4))) //player is above of enemy
-				{
-					this.collisionSound.play();
-					if(this.player.lives === 0)
-					{
-					    this.bgSound.stop();
-						return true;
+        for(var i = 0; i < this.enemies.length; i++){
+        	console.log(this.enemies[i]);
+					console.log(this.player);
+					console.log("Printing This: " + this);
+					//check for collision
+					if(this.enemies[i] !== undefined) {
+						if(this.colission(this.player,this.enemies[i],PLAYER_WIDTH,PLAYER_HEIGHT,ENEMY_WIDTH,ENEMY_HEIGHT))
+						{
+							this.collisionSound.play();
+							delete this.enemies[i];
+
+							if(this.player.lives === 0){
+								this.bgSound.stop();
+								return true;
+							}
+							else {
+								console.log(this.player.lives);
+								this.player.lives = this.player.lives - 1;
+								console.log(this.player.lives);
+								return false;
+							}
+						}
 					}
-					else
-					{
-					    console.log(this.player.lives);
-					    this.player.lives = this.player.lives - 1;
-					    console.log(this.player.lives);
-					    return false;
-					}
-				}
-            }
         }
         return false;
     }
